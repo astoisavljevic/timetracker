ab47167feccd4c68abe6bd4956c820827c045bde
Aleksandar Stoisavljevic <staleks@gmail.com>
2013-04-06 14:48:51 +0200

    [update:] improve code quality

--- core/src/test/java/rs/in/staleksit/timetracker/core/dto/UserDTOTest.java
+++ core/src/test/java/rs/in/staleksit/timetracker/core/dto/UserDTOTest.java
@@ -36,5 +36,50 @@ public class UserDTOTest {
 		
 		assertNotNull(userDTO);
 	}
+	
+	public void testUsername() {
+		userDTO = new UserDTO();
+		userDTO.setUsername("username");
+		
+		assertNotNull(userDTO);
+		
+		assertEquals(userDTO.getUsername(), "username");
+	}
+	
+	
+	public void testEmail() {
+		userDTO = new UserDTO();
+		userDTO.setEmail("test@test.com");
+		
+		assertNotNull(userDTO);
+		
+		assertEquals(userDTO.getEmail(), "test@test.com");
+		
+	}
+	
+	public void testFirstname() {
+		userDTO = new UserDTO();
+		userDTO.setFirstName("test");
+		
+		assertNotNull(userDTO);
+		
+		assertEquals(userDTO.getFirstName(), "test");
+	}
+	
+	public void testLastname() {
+		userDTO = new UserDTO();
+		userDTO.setLastName("name");
+		
+		assertNotNull(userDTO);
+		
+		assertEquals(userDTO.getLastName(), "name");
+	}
+	
+	
+	public void testToString() {
+		userDTO = new UserDTO();
+		userDTO.setEmail("test@test.com");
 
+		assertTrue(userDTO.toString().contains("email=test@test.com"));
+	}
 }
